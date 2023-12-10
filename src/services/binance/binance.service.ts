b84f8c75b3dd106ee4cce3fa43c27d85ae7a20e3
Ilkin Mammadli <ilkinmammadli01@gmail.com>
2023-12-10 14:11:30 +0400

    feat: create wallet info method

--- src/services/binance/binance.service.ts
+++ src/services/binance/binance.service.ts
@@ -1,13 +1,14 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { AxiosResponse } from "axios";
 import { Spot, UserAssetsResponse } from "@binance/connector";
+import { IBinanceService } from "./binance.interface";
 
 @Injectable()
-export class BinanceService {
+export class BinanceService implements IBinanceService {
     constructor(@Inject("BinanceClient") private readonly binanceClient: Spot) {}
 
-    async fetchAccountInfo(): Promise<AxiosResponse<UserAssetsResponse>> {
-        const response: AxiosResponse<UserAssetsResponse> = await this.binanceClient.userAsset();
+    async fetchWalletInfo(): Promise<AxiosResponse<UserAssetsResponse[]>> {
+        const response: AxiosResponse<UserAssetsResponse[]> = await this.binanceClient.userAsset();
         console.log(response);
         return response;
     }
